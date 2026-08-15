8ca67acf3272390cc54614d4d3418fe90ab57c16
Maxwell Dev <268078768+Asvyr@users.noreply.github.com>
2026-08-15 15:45:27 -0500

    Stop play moving in menu

--- Characters/Player/Player.cs
+++ Characters/Player/Player.cs
@@ -95,6 +95,7 @@ public partial class Player : CharacterBody2D
 
 	public override void _PhysicsProcess(double delta)
 	{
+		if (InMenu) { return; }
 		if (!Swimming) { Walk(delta); }
 		else { Swim(delta); }
 		MoveAndSlide();
