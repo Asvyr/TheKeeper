157397de0c1547146275fb7c1194697aec9b2bd5
Error <Jmwebb2635@gmail.com>
2026-08-15 21:53:16 -0500

    Changes???

--- Scenes/SceneManager.cs
+++ Scenes/SceneManager.cs
@@ -3,31 +3,31 @@ using System;
 
 public partial class SceneManager : Node2D
 {
-    [Export] MainMenu MainMenuUI;
-    [Export] PackedScene PlayScene;
-    [Export] PackedScene IntroScene;
-    [Export] AnimationPlayer FadeAnim;
+	[Export] MainMenu MainMenuUI;
+	[Export] PackedScene PlayScene;
+	[Export] PackedScene IntroScene;
+	[Export] AnimationPlayer FadeAnim;
 
-    private string CurrentScene = "mainmenu";
+	private string CurrentScene = "mainmenu";
 
-    public void ChangeScene(string scene)
-    {
-        CurrentScene = scene;
+	public void ChangeScene(string scene)
+	{
+		CurrentScene = scene;
 
-        switch (scene)
-        {
-            case "intro":
-                FadeAnim.Play("FadeIn");
-                break;
-        }
-    }
-    
-    public void UpdateScene()
-    {
-        MainMenuUI.Visible = false;
-        Play playScene = PlayScene.Instantiate<Play>();
-        AddChild(playScene);
+		switch (scene)
+		{
+			case "intro":
+				FadeAnim.Play("FadeIn");
+				break;
+		}
+	}
+	
+	public void UpdateScene()
+	{
+		MainMenuUI.Visible = false;
+		Play playScene = PlayScene.Instantiate<Play>();
+		AddChild(playScene);
 
-        FadeAnim.Play("FadeOut");
-    }
+		FadeAnim.Play("FadeOut");
+	}
 }
