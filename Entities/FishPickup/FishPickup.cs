47cdb872bb33edc9bd69e4a79944d6667779e4d8
Maxwell Dev <268078768+Asvyr@users.noreply.github.com>
2026-08-16 13:52:57 -0500

    Art and particles

--- Entities/FishPickup/FishPickup.cs
+++ Entities/FishPickup/FishPickup.cs
@@ -5,10 +5,12 @@ public partial class FishPickup : Node2D
 {
 	[Export] public ItemResource FishItem;
 	[Export] public Sprite2D sprite;
+	[Export] public GpuParticles2D Particle;
 
 	public override void _Ready()
 	{
 		sprite.Texture = FishItem.Thumbnail;
+		Particle.Emitting = true;
 	}
 
 	public void _BodyEntered(Node2D body)
