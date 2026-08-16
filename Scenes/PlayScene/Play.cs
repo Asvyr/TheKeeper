5b0449081114d19356ac17549d838053c12c659d
Maxwell Dev <268078768+Asvyr@users.noreply.github.com>
2026-08-15 21:44:38 -0500

    Added End Of Day Screen

--- Scenes/PlayScene/Play.cs
+++ Scenes/PlayScene/Play.cs
@@ -1,7 +1,34 @@
+using System.Text;
 using Godot;
 
 public partial class Play : Node2D
 {
+    [Export] public EndOfDayMenu endOfDayMenu;
+    [Export] public LightHouse lightHouse;
+    [Export] public int Strikes = 0;
+
+    public void _DayFinished()
+    {
+        if (lightHouse.CurrentOil >= lightHouse.RequiredOil)
+        {
+            endOfDayMenu.Details.Text = $"The Lighthouse remains on overnight!\nThe Boats navigate safely!";
+        }
+        else
+        {
+            Strikes += 1;
+            endOfDayMenu.Details.Text = $"The lighthouse dosen't have enough fuel for the night!\nYou needed {lightHouse.RequiredOil - lightHouse.CurrentOil} more [img]res://Art Assets/Sprite-oil.png[/img]Fish Oil!";
+        }
+
+        StringBuilder strikeString = new StringBuilder();
+        for (int i = 0; i < Strikes; i++)
+        {
+            strikeString.Append("[img=32x32]res://Art Assets/X.png[/img]");
+        }
+        endOfDayMenu.Strikes.Text = $"Strikes: {strikeString}";
+
+
+        endOfDayMenu.Play();
+    }
 
     public void _BodyEntered(Node2D body)
     {
@@ -11,7 +38,7 @@ public partial class Play : Node2D
         }
 
     }
-    
+
     public void _BodyExited(Node2D body)
     {
         if (body.HasMethod("UpdateSwimming"))
@@ -19,4 +46,10 @@ public partial class Play : Node2D
             body.Call("UpdateSwimming", true);
         }
     }
+
+    public override void _Ready()
+    {
+        endOfDayMenu = GetTree().Root.GetNode<EndOfDayMenu>("Main/CanvasLayer/EndOfDayMenu");
+    }
+
 }
---
+++ UI/EndOfDay/EndOfDayMenu.cs
@@ -0,0 +1,16 @@
+using Godot;
+using Godot.Collections;
+
+public partial class EndOfDayMenu : Control
+{
+	[Export] public RichTextLabel Details;
+	[Export] public RichTextLabel Strikes;
+	[Export] AnimationPlayer anims;
+
+	public void Play()
+	{
+		Visible = true;
+		anims.Play("FadeIn");
+    }
+
+}
